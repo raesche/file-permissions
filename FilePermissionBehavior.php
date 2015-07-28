38cd3c6a6d595d311f213bb6b577afa014a2bc05
raesche <rashid.talib@rocketmail.com>
2015-07-28 15:30:03 +0200

    initial
    initial

---
+++ FilePermissionBehavior.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+namespace Propel\Generator\Behavior\FilePermission;
+
+use Propel\Generator\Builder\Om\QueryBuilder;
+use Propel\Generator\Model\Behavior;
+use Propel\Generator\Model\Column;
+use Propel\Generator\Model\ForeignKey;
+use Propel\Generator\Util\PhpParser;
+use Propel\Runtime\Exception\PropelException;
+
+
+class FilePermissionBehavior extends Behavior
+{
+  // default parameters value
+  protected $parameters = array(
+    'name' => null,
+  );
+}
